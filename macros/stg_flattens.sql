e04d4ae9c37af81e319231cb496e73bfcef87868
Jules <jalvord19@gmail.com>
2022-11-16 14:27:09 -0600

    var reference

--- macros/stg_flattens.sql
+++ macros/stg_flattens.sql
@@ -24,12 +24,12 @@ flattened as (
         {{key}},
         {% endfor %}
         {% for col in array_cols %}
-        {% if 'Descriptor' in col[0] %}
-        {{ extract_descriptor(col[0]) }}
-        {% else %}
-        {{col[0]}}
-        {% endif %}
-        as col[1]
+            {% if 'Descriptor' in col[0] %}
+                {{ extract_descriptor(col[0]) }}
+            {% else %}
+                {{col[0]}}
+            {% endif %}
+        as {{col[1]}}
         {%- if not loop.last %},{% endif -%}
         {% endfor %}
     from stg
