d2023d5a7c2b4f3cfa59620ec643ea9addd141ef
Jules <jalvord19@gmail.com>
2022-11-16 15:28:02 -0600

    worse output but easier to read

--- macros/stg_flattens.sql
+++ macros/stg_flattens.sql
@@ -28,15 +28,14 @@ flattened as (
         tenant_code,
         api_year,
         {%- for key in keys %}
-        {{key}},
+            {{key}},
         {% endfor %}
         {%- for col in array_cols %}
-        {%- if 'Descriptor' in col[0] %}
-        {{ extract_descriptor(col[0]) }}
-        {% else %}
-        {{col[0]}}
-        {% endif -%} 
-        as {{col[1]}}{%- if not loop.last %},{% endif -%}
+            {%- if 'Descriptor' in col[0] %}
+                {{ extract_descriptor(col[0]) }}
+            {% else %}
+                {{col[0]}}
+        {% endif -%} as {{col[1]}}{%- if not loop.last %},{% endif -%}
         {% endfor %}
     from stg
         , lateral flatten(input=>{{array_name}})
