94c53cf2464305b98e4387428bf34fbd81e189fe
Jules <jalvord19@gmail.com>
2022-11-16 14:23:39 -0600

    add column aliases

--- macros/stg_flattens.sql
+++ macros/stg_flattens.sql
@@ -2,10 +2,10 @@
 
 {% set array_defs = {
         'v_electronic_mails': {
-            'cols': ["value:electronicMailTypeDescriptor::string",
-                     "lower(value:electronicMailAddress::string)",
-                     "value:primaryEmailAddressIndicator::boolean",
-                     "value:doNotPublishIndicator::boolean"]
+            'cols': [["value:electronicMailTypeDescriptor::string", 'email_type'],
+                     ["lower(value:electronicMailAddress::string)", 'email_address'],
+                     ["value:primaryEmailAddressIndicator::boolean", 'is_primary_email']
+                     ["value:doNotPublishIndicator::boolean", 'do_not_publish']]
              },
     }
 %}
@@ -24,11 +24,12 @@ flattened as (
         {{key}},
         {% endfor %}
         {% for col in array_cols %}
-        {% if 'Descriptor' in col %}
-        {{ extract_descriptor(col) }}
+        {% if 'Descriptor' in col[0] %}
+        {{ extract_descriptor(col[0]) }}
         {% else %}
-        {{col}}
+        {{col[0]}}
         {% endif %}
+        as col[1]
         {%- if not loop.last %},{% endif -%}
         {% endfor %}
     from stg
