e2f3b2ac61590c721233e7405d12b1822200d57a
Jules <jalvord19@gmail.com>
2022-11-16 13:36:45 -0600

    first pass at flatten macro

---
+++ macros/flatten_emails.sql
@@ -0,0 +1,22 @@
+{% macro flatten_emails(stg_ref, keys) %}
+
+with stg as (
+    select * from {{ ref(stg_ref) }}
+),
+flattened as (
+    select 
+        tenant_code,
+        api_year,
+        {% for key in keys %}
+        key{%- if not loop.last %},{% endif -%}
+        {% endfor %}
+        k_parent,
+        {{ extract_descriptor('value:electronicMailTypeDescriptor::string') }} as email_type,
+        lower(value:electronicMailAddress::string) as email_address,
+        value:primaryEmailAddressIndicator::boolean as is_primary_email,
+        value:doNotPublishIndicator::boolean as do_not_publish
+    from stg
+        , lateral flatten(input=>v_electronic_mails)
+)
+select * from flattened
+{% endmacro %}
