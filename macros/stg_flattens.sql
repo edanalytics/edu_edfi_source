df1a4b5632f7b952db5f6780b35606205e1ae8dd
Jules <jalvord19@gmail.com>
2022-11-16 15:26:15 -0600

    one more try to make prettier

--- macros/stg_flattens.sql
+++ macros/stg_flattens.sql
@@ -27,15 +27,16 @@ flattened as (
     select 
         tenant_code,
         api_year,
-        {% for key in keys %}
+        {%- for key in keys %}
         {{key}},
         {% endfor %}
-        {% for col in array_cols %}
-        {% if 'Descriptor' in col[0] %}
+        {%- for col in array_cols %}
+        {%- if 'Descriptor' in col[0] %}
         {{ extract_descriptor(col[0]) }}
         {% else %}
         {{col[0]}}
-        {% endif -%} as {{col[1]}}{%- if not loop.last %},{% endif -%}
+        {% endif -%} 
+        as {{col[1]}}{%- if not loop.last %},{% endif -%}
         {% endfor %}
     from stg
         , lateral flatten(input=>{{array_name}})
