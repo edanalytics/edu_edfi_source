ba6d66f0dbfc12d381e507896c32cf47780fd47b
keenzarate213 <kzarate@edanalytics.org>
2022-11-18 14:54:39 -0600

    update stage models

--- models/staging/edfi_3/stage/stg_ef3__descriptors.sql
+++ models/staging/edfi_3/stage/stg_ef3__descriptors.sql
@@ -1,5 +1,25 @@
 with base_descriptors as (
-    select *
-    from {{ ref('base_ef3__descriptors') }}
+    select * from {{ ref('base_ef3__descriptors') }}
+),
+keyed as (
+    select 
+        {{ dbt_utils.surrogate_key(
+            ['tenant_code',
+            'api_year',
+            'descriptor_name',
+            'code_value']
+        ) }} as k_descriptor, 
+        api_year as school_year,
+        base_descriptors.*
+    from base_descriptors
+),
+deduped as (
+    {{
+        dbt_utils.deduplicate(
+            relation='keyed',
+            partition_by='k_descriptor',
+            order_by='pull_timestamp desc')
+    }}
 )
-select * from base_descriptors
+select * from deduped
+
--- models/staging/edfi_3/stage/stg_ef3__stu_ed_org__languages.sql
+++ models/staging/edfi_3/stage/stg_ef3__stu_ed_org__languages.sql
@@ -1,10 +1,6 @@
 with stg_stu_ed_org as (
     select * from {{ ref('stg_ef3__student_education_organization_associations') }}
 ),
-stg_descriptors as (
-    select * from {{ ref('stg_ef3__descriptors')}}
-    where descriptor_name = 'language_descriptors'
-),
 flattened as (
     select 
         tenant_code,
@@ -19,27 +15,5 @@ flattened as (
     from stg_stu_ed_org
         , lateral flatten(input=>v_languages) as lang
         , lateral flatten(input=>lang.value:uses) as lang_uses
-),
--- pick home language if exists
-choose_language_use as (
-    {{ row_pluck('flattened',
-                key='k_student',
-                column='language_use',
-                preferred='Home language') }}
 )
-select 
-    distinct
-        ft.tenant_code,
-        ft.api_year,
-        ft.k_student,
-        ft.k_student_xyear,
-        ft.ed_org_id,
-        ft.k_lea,
-        ft.k_school,
-        descr.short_description as language_name, 
-        choose_language_use.language_use
-from flattened ft
-left join choose_language_use on ft.k_student = choose_language_use.k_student
-left join stg_descriptors descr on  ft.tenant_code = descr.tenant_code 
-                                and ft.api_year   = descr.api_year
-                                and ft.code_value = descr.code_value
+select * from flattened
