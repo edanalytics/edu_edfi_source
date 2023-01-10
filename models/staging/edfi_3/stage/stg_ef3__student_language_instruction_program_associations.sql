a47808eaa02346b24afa87d9f352619c5084bc07
jkaiser <jayckaiser@gmail.com>
2023-01-10 14:26:19 -0600

    Add base and stage models for studentLanguageInstructionProgramAssociations; add referential integrity test for main stage model.

---
+++ models/staging/edfi_3/stage/stg_ef3__student_language_instruction_program_associations.sql
@@ -0,0 +1,28 @@
+with base_stu_programs as (
+    select *
+    from {{ ref('base_ef3__student_language_instruction_program_associations') }}
+    where not is_deleted
+),
+
+keyed as (
+    select 
+        {{ gen_skey('k_student') }},
+        {{ gen_skey('k_student_xyear') }},
+        {{ gen_skey('k_program') }},
+        {{ edorg_ref() }},
+        api_year as school_year,
+        base_stu_programs.*
+        {{ extract_extension(model_name=this.name, flatten=True) }}
+
+    from base_stu_programs
+),
+
+deduped as (
+    {{ dbt_utils.deduplicate(
+        relation='keyed',
+        partition_by='k_student, k_program, program_enroll_begin_date, delivering_ed_org_id, school_year',
+        order_by='pull_timestamp desc'
+    ) }}
+)
+
+select * from deduped
