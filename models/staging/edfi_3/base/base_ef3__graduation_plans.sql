d2485875b9e4b2c5f8c3d456e3d53f9a5df6bf3e
ejoranlien <ejoranlien@edanalytics.org>
2023-01-23 15:08:25 -0600

    minor cleanup

--- models/staging/edfi_3/base/base_ef3__graduation_plans.sql
+++ models/staging/edfi_3/base/base_ef3__graduation_plans.sql
@@ -10,21 +10,20 @@ renamed as (
         filename,
         is_deleted,
         v:id::string as record_guid,
-        v:educationOrganizationReference:educationOrganizationId::int as ed_org_id,
-        v:educationOrganizationReference:link:rel::string             as ed_org_type,
+        v:educationOrganizationReference:educationOrganizationId::int           as ed_org_id,
+        v:educationOrganizationReference:link:rel::string                       as ed_org_type,
         v:graduationSchoolYearTypeReference:schoolYear::int                     as graduation_school_year,
         {{ extract_descriptor('v:graduationPlanTypeDescriptor::string') }}      as graduation_plan_type,
         {{ extract_descriptor('v:totalRequiredCreditTypeDescriptor::string') }} as total_required_credit_type,
         v:totalRequiredCreditConversion::float                                  as total_required_credit_conversion,
         v:totalRequiredCredits::float                                           as total_required_credits,
-        v:individualPlan::boolean                                               as individual_plan,
+        v:individualPlan::boolean                                               as is_individual_plan,
         -- lists
         v:creditsByCreditCategories as v_credits_by_credit_categories,
         v:creditsByCourses          as v_credits_by_courses,
         v:creditsBySubjects         as v_credits_by_subjects,   
         v:requiredAssessments       as v_required_assessments,
         --references
-        v:graduationSchoolYearTypeReference as graduation_school_year_type_reference,
         v:educationOrganizationReference    as education_organization_reference,
 
         -- edfi extensions
