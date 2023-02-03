63aff28a723cce4017e7f7a395ad63b074076aff
jkaiser <jayckaiser@gmail.com>
2023-02-03 13:48:15 -0600

    Standardize vanilla student program associations to match the others.

--- models/staging/edfi_3/base/base_ef3__student_program_associations.sql
+++ models/staging/edfi_3/base/base_ef3__student_program_associations.sql
@@ -1,40 +1,48 @@
-with stu_prog as (
+with source_stu_programs as (
     {{ source_edfi3('student_program_associations') }}
 ),
+
 renamed as (
     select
+        -- generic columns
         tenant_code,
         api_year,
         pull_timestamp,
         file_row_number,
         filename,
         is_deleted,
-        v:id::string as record_guid,
+
+        v:id::string                                                      as record_guid,
         v:studentReference:studentUniqueId::string                        as student_unique_id,
-        v:educationOrganizationReference:educationOrganizationId::integer as delivering_ed_org_id,
-        v:educationOrganizationReference:link:rel::string                 as delivering_ed_org_type,
+        v:educationOrganizationReference:educationOrganizationId::integer as ed_org_id,
+        v:educationOrganizationReference:link:rel::string                 as ed_org_type,
         v:programReference:educationOrganizationId::integer               as program_ed_org_id,
-        v:beginDate::date                           as program_enroll_begin_date,
-        v:endDate::date                             as program_enroll_end_date,
-        v:programReference:programName::string      as program_name,
-        v:servedOutsideOfRegularSession::boolean    as served_outside_regular_session,
-        v:participationStatus:designatedBy::string  as status_designated_by,
-        v:participationStatus:statusBeginDate::date as status_begin_date,
-        v:participationStatus:statusEndDate::date   as status_end_date,
+        v:beginDate::date                                                 as program_enroll_begin_date,
+        v:endDate::date                                                   as program_enroll_end_date,
+        v:programReference:programName::string                            as program_name,
+        v:servedOutsideOfRegularSession::boolean                          as is_served_outside_regular_session,
+        v:participationStatus:designatedBy::string                        as participation_status_designated_by,
+        v:participationStatus:statusBeginDate::date                       as participation_status_begin_date,
+        v:participationStatus:statusEndDate::date                         as participation_status_end_date,
+
         -- descriptors
         {{ extract_descriptor('v:participationStatus:participationStatusDescriptor::string') }} as participation_status,
-        {{ extract_descriptor('v:programReference:programTypeDescriptor::string') }} as program_type,
-        {{ extract_descriptor('v:reasonExitedDescriptor::string') }}                 as reason_exited,
+        {{ extract_descriptor('v:programReference:programTypeDescriptor::string') }}            as program_type,
+        {{ extract_descriptor('v:reasonExitedDescriptor::string') }}                            as reason_exited,
+
         -- references
-        v:studentReference               as student_reference,
-        v:programReference               as program_reference,
         v:educationOrganizationReference as education_organization_reference,
-        -- lists 
-        v:services                     as v_services,
+        v:programReference               as program_reference,
+        v:studentReference               as student_reference,
+
+        -- lists
         v:programParticipationStatuses as v_program_participation_statuses,
+        v:services                     as v_services,
 
         -- edfi extensions
         v:_ext as v_ext
-    from stu_prog
+
+    from source_stu_programs
 )
+
 select * from renamed
