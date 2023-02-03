ff1de167d60bc71f020982a3dea598a596d1aefb
jkaiser <jayckaiser@gmail.com>
2023-02-03 11:28:23 -0600

    Standardize all stg_ef3__student_*_program_services models.

--- models/staging/edfi_3/stage/stg_ef3__stu_spec_ed__program_services.sql
+++ models/staging/edfi_3/stage/stg_ef3__stu_spec_ed__program_services.sql
@@ -1,6 +1,7 @@
-with stage_stu_spec_ed as (
+with stage_stu_programs as (
     select * from {{ ref('stg_ef3__student_special_education_program_associations') }}
 ),
+
 flattened as (
     select 
         tenant_code,
@@ -10,28 +11,28 @@ flattened as (
         k_program,
         k_lea,
         k_school,
-        spec_ed_program_begin_date,
-        {{ extract_descriptor('value:specialEducationProgramServiceDescriptor::string') }} as special_education_program_service,
+
+        {{ extract_descriptor('value:specialEducationProgramServiceDescriptor::string') }} as program_service,
         value:primaryIndicator::boolean as primary_indicator,
         value:providers                 as v_providers,
         value:serviceBeginDate::date    as service_begin_date,
         value:serviceEndDate::date      as service_end_date,
+
         -- edfi extensions
         value:_ext as v_ext        
 
-    from stage_stu_spec_ed
-        , lateral flatten(input=>v_special_education_program_services) 
+    from stage_stu_programs,
+        lateral flatten(input => v_special_education_program_services)
 ),
 
 -- There is a v_ext nested within v_special_education_program_services. Those extensions must be extracted here, not in prev CTE, 
 -- because dbt gets confused whether to reference stage_stu_spec_ed.v_ext or v_special_education_program_services.v_ext
 extended as (
     select 
-      flattened.*
-      {{ extract_extension(model_name=this.name, flatten=True) }}
-    from flattened
+        flattened.*
+        {{ extract_extension(model_name=this.name, flatten=True) }}
 
+    from flattened
 )
 
 select * from extended
-
