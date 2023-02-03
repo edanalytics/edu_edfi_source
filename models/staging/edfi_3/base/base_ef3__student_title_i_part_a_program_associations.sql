28a65a24fd8c9312ec1663dfa16556929597d4f7
jkaiser <jayckaiser@gmail.com>
2023-02-03 13:47:56 -0600

    Remove 'delivering_' prefix from ed_org reference columns across all program association models.

--- models/staging/edfi_3/base/base_ef3__student_title_i_part_a_program_associations.sql
+++ models/staging/edfi_3/base/base_ef3__student_title_i_part_a_program_associations.sql
@@ -14,8 +14,8 @@ renamed as (
 
         v:id::string                                                      as record_guid,
         v:studentReference:studentUniqueId::string                        as student_unique_id,
-        v:educationOrganizationReference:educationOrganizationId::integer as delivering_ed_org_id,
-        v:educationOrganizationReference:link:rel::string                 as delivering_ed_org_type,
+        v:educationOrganizationReference:educationOrganizationId::integer as ed_org_id,
+        v:educationOrganizationReference:link:rel::string                 as ed_org_type,
         v:programReference:educationOrganizationId::integer               as program_ed_org_id,
         v:beginDate::date                                                 as program_enroll_begin_date,
         v:endDate::date                                                   as program_enroll_end_date,
