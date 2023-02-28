86de6c21cdfd2b618b6b6aa7578aa630f7298dd9
Jules <jalvord19@gmail.com>
2023-02-28 12:32:13 -0600

    union all deprecated version

--- models/staging/edfi_3/stage/stg_ef3__discipline_actions__student_discipline_incident_behaviors.sql
+++ models/staging/edfi_3/stage/stg_ef3__discipline_actions__student_discipline_incident_behaviors.sql
@@ -14,5 +14,20 @@ flattened as (
         value:studentDisciplineIncidentBehaviorAssociationReference:studentUniqueId::string as student_unique_id
     from stg_discipline_actions,
         lateral flatten(input => v_student_discipline_incident_behavior_associations)
+
+    union all
+
+    select 
+        tenant_code,
+        api_year,
+        discipline_action_id,
+        discipline_date,
+        k_student,
+        null as behavior_type,
+        value:studentDisciplineIncidentAssociationReference:incidentIdentifier::string as incident_id,
+        value:studentDisciplineIncidentAssociationReference:schoolId::string as school_id,
+        value:studentDisciplineIncidentAssociationReference:studentUniqueId::string as student_unique_id
+    from stg_discipline_actions,
+        lateral flatten(input => v_student_discipline_incident_associations)
 )
 select * from flattened
