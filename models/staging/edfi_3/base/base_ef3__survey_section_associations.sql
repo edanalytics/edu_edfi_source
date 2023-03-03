8eeecfac3a1bdbfa092a74f9177841065d16c095
Jules <jalvord19@gmail.com>
2023-03-03 11:58:15 -0600

    first draft of survey sections

---
+++ models/staging/edfi_3/base/base_ef3__survey_section_associations.sql
@@ -0,0 +1,28 @@
+with survey_sections as (
+    {{ source_edfi3('survey_section_associations') }}
+),
+renamed as (
+    select 
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+
+        v:id::string                                                  as record_guid,
+        v:sectionReference:localCourseCode::string                    as local_course_code,
+        v:sectionReference:schoolId::string                           as school_id,
+        v:sectionReference:schoolYear::string                         as school_year,
+        v:sectionReference:sectionIdentifier::string                  as section_id,
+        v:sectionReference:sessionName::string                        as session_name,
+        v:surveyReference:surveyIdentifier::string                    as survey_id,
+        v:surveyReference:namespace::string                           as namespace,
+        --references
+        v:sectionReference as section_reference,
+        v:surveyReference  as survey_reference,
+        -- edfi extensions
+        v:_ext as v_ext
+    from survey_sections
+)
+select * from renamed
