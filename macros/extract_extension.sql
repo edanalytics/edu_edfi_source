ae7478c2fe9bfbf760a3545c4da710749a36afc1
jkaiser <jayckaiser@gmail.com>
2023-03-29 16:47:02 -0500

    Extend extract_extension() macro to accept a list of relation names or a singleton relation name.

--- macros/extract_extension.sql
+++ macros/extract_extension.sql
@@ -6,25 +6,39 @@
   -- for each extension, just return the col name
 #}
 {% macro extract_extension(model_name, flatten) %}
-{%- set extensions =  var('extensions')[model_name]  -%}
+
+{% if model_name is string %}
+    {% set model_name = [model_name] %}
+{% endif %}
+
+{% set extensions = [] -%}
+{%- for relation in model_name -%}
+    {%- for extension in var('extensions')[relation] -%}
+        {%- do extensions.append(extension) -%}
+    {%- endfor -%}
+{%- endfor %}
 
 {%- if extensions is defined and extensions|length > 0 -%},{% endif -%}
 
-{%- for ext in extensions %}
-  {%- set ext_native_name =  extensions[ext].name  -%}
-  {%- set full_ext_native_name =  'v_ext:' + ext_native_name  -%}
-  {%- set ext_dtype =  extensions[ext].dtype  -%}
-  {%- set ext_extract_descriptor =  extensions[ext].extract_descriptor  -%}
-  {%- if flatten %}
-    {%- if ext_extract_descriptor %}
-      {{extract_descriptor(full_ext_native_name)}}::{{ext_dtype}} as {{ext}}
+{%- for ext in extensions|unique %}
+    {%- set ext_native_name =  extensions[ext].name  -%}
+    {%- set full_ext_native_name =  'v_ext:' + ext_native_name  -%}
+    {%- set ext_dtype =  extensions[ext].dtype  -%}
+    {%- set ext_extract_descriptor =  extensions[ext].extract_descriptor -%}
+
+    {%- if flatten %}
+        {%- if ext_extract_descriptor %}
+            {{extract_descriptor(full_ext_native_name)}}::{{ext_dtype}} as {{ext}}
+        {%- else %}
+            {{full_ext_native_name}}::{{ext_dtype}} as {{ext}}
+        {%- endif %}
+
     {%- else %}
-      {{full_ext_native_name}}::{{ext_dtype}} as {{ext}}
-    {%- endif %}    
-  {%- else %}
-    {{ext}}
-  {%- endif %}
-  {%- if not loop.last %},{% endif -%}
+        {{ext}}
+
+    {%- endif %}
+    {%- if not loop.last %},{% endif -%}
+
 {%- endfor -%}
 
 {% endmacro %}
