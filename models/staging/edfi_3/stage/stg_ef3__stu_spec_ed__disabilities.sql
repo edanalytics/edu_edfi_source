09123856eb59f44b3c3e6cc4c0b2ef11561f6293
Ryan Aguilar <raguilar@edanalytics.org>
2023-05-09 22:54:47 -0500

    add stg_ef3__stu_spec_ed__disabilities model

---
+++ models/staging/edfi_3/stage/stg_ef3__stu_spec_ed__disabilities.sql
@@ -0,0 +1,25 @@
+with stg_stu_ed_org as (
+    select * from {{ ref('stg_ef3__student_special_education_program_associations') }}
+),
+flattened as (
+    select
+        tenant_code,
+        api_year,
+        k_student,
+        k_student_xyear,
+        ed_org_id,
+        k_lea,
+        k_school,
+        {{ extract_descriptor('disab.value:disabilityDescriptor::string') }} as disability_type,
+        {{ extract_descriptor('disab.value:disabilityDeterminationSourceTypeDescriptor::string') }} as disability_source_type,
+        disab.value:disabilityDiagnosis::string as disability_diagnosis,
+        disab.value:orderOfDisability::int as order_of_disability,
+        -- todo: perhaps these would better serve as wide booleans
+        -- in which case we would not want to double-flatten, but leave nested
+        -- for a downstream step
+        {{ extract_descriptor('desig.value:disabilityDesignationDescriptor::string') }} as disability_designation
+    from stg_stu_ed_org
+        , lateral flatten(input=>v_disabilities) disab
+        , lateral flatten(input=>disab.value:designations, OUTER => TRUE) as desig
+)
+select * from flattened
