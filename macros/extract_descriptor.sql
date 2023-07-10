e9edd92a94ed75888f3ea9120cdcec5a9c379ad1
rlittle08 <rlittle@edanalytics.org>
2023-07-10 14:02:10 -0500

    whitespace and comments

--- macros/extract_descriptor.sql
+++ macros/extract_descriptor.sql
@@ -1,27 +1,29 @@
 -- grab descriptor codes from namespaced descriptor values
--- TODO decide how data should be returned, some options:
---   a) one value, either code value, long, or short description, based on config
---   b) if configured, create all three columns (sometimes only two?)
---   c) one value either code value, or nested json object with all three, based on config 
 {% macro extract_descriptor(col) -%}
     
     {%- set stripped_col = col.split(":")[1] -%}
     {%- set config = var('descriptors')[stripped_col] or None -%}
-    {%- set include_descriptions = config['include_descriptions'] or False -%}
+    {%- set replace_with_short_descr = config['replace_with_short_descr'] or False -%}
     {%- set alias = config['alias'] -%}
 
-    {%- if not include_descriptions -%}
+    -- if not configured to replace (default), split part from raw value
+    {%- if not replace_with_short_descr -%}
+
       split_part({{ col }}, '#', -1)
-    {%- elif include_descriptions and not alias -%}
-      {{ exceptions.raise_compiler_error("Error in extract_descriptor(): if include_descriptions is set, must define an alias") }}
+
+    {%- elif replace_with_short_descr and not alias -%}
+
+      {{ exceptions.raise_compiler_error("Error in extract_descriptor(): if replace_with_short_descr is set, must define an alias") }}
+
+    -- if configured to replace, query int_ef3__deduped_descriptors to find each value's short description
     {%- else %}
-      -- TODO split this into a separate macro below, once we decide how data should be returned
       
       {% set query_descriptors -%}
         select namespace, code_value, description, short_description
         from {{ ref('int_ef3__deduped_descriptors') }}
         where lower(split_part(namespace, '/', -1)) = lower('{{stripped_col}}')
       {%- endset -%}
+
       {%- set descriptor_xwalk = run_query(query_descriptors) %}
 
       {%- if execute -%}
@@ -31,10 +33,13 @@
         {%- set short_descriptions = descriptor_xwalk.columns[3].values() -%}
       {%- endif -%}
       
+      -- create a case/when statement that replaces each raw code_value with short_description
       case
-      {%- for i in range(code_values|length) %}
+      {%- for i in range(code_values|length) -%}
+
         when split_part({{ col }}, '#', 1) = '{{namespaces[i]}}' and split_part({{ col }}, '#', -1) = '{{code_values[i]}}'
           then '{{short_descriptions[i]}}'
+
       {%- endfor %}
         end       
     {%- endif -%}
