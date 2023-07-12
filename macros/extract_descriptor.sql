981172bdc458636e39cb769568529809400c14a8
rlittle08 <rlittle@edanalytics.org>
2023-07-12 15:50:58 -0500

    fix for multiple :s, and for extension descriptors. plus expand to replace_with

--- macros/extract_descriptor.sql
+++ macros/extract_descriptor.sql
@@ -1,12 +1,12 @@
 -- grab descriptor codes from namespaced descriptor values
 {% macro extract_descriptor(col) -%}
     
-    {%- set stripped_col = col.split(":")[1] -%}
+    {%- set stripped_col = col.split(":")[-3] -%}
     {%- set config = var('descriptors')[stripped_col] or None -%}
-    {%- set replace_with_short_descr = config['replace_with_short_descr'] or False -%}
+    {%- set replace_with = config['replace_with'] or None -%}
 
     -- if not configured to replace (default), split part from raw value
-    {%- if not replace_with_short_descr %}
+    {%- if not replace_with %}
 
       split_part({{ col }}, '#', -1)
 
@@ -14,7 +14,7 @@
     -- if configured to replace, query int_ef3__deduped_descriptors to find each value's short description
       
       {% set query_descriptors -%}
-        select namespace, code_value, description, short_description
+        select namespace, code_value, {{ replace_with }}
         from {{ ref('int_ef3__deduped_descriptors') }}
         where lower(split_part(namespace, '/', -1)) = lower('{{stripped_col}}')
       {%- endset -%}
@@ -25,7 +25,6 @@
         {%- set namespaces = descriptor_xwalk.columns[0].values() -%}
         {%- set code_values = descriptor_xwalk.columns[1].values() -%}
         {%- set descriptions = descriptor_xwalk.columns[2].values() -%}
-        {%- set short_descriptions = descriptor_xwalk.columns[3].values() -%}
       {%- endif -%}
       
       -- create a case/when statement that replaces each raw code_value with short_description
@@ -33,9 +32,9 @@
       {% for i in range(code_values|length) -%}
 
         when split_part({{ col }}, '#', 1) = '{{namespaces[i]}}' and split_part({{ col }}, '#', -1) = '{{code_values[i]}}'
-          then '{{short_descriptions[i]}}'
+          then '{{descriptions[i]}}'
 
-      {%- endfor %}
+      {% endfor %}
         -- default to raw value if not found in descriptors table
         else split_part({{ col }}, '#', -1)
         end       
