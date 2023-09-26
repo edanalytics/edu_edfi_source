f89edb68a53e34e6dbe80b78e49cb797d0b11cfe
Angelica Lastra <angelicaalastra@gmail.com>
2023-09-26 13:49:02 -0700

    named columns clearer

--- models/staging/edfi_3/stage/stg_ef3__grades__learning_standards.sql
+++ models/staging/edfi_3/stage/stg_ef3__grades__learning_standards.sql
@@ -12,8 +12,8 @@ flattened as (
         stg_grades.tenant_code,
         stg_grades.api_year,
         v_lsg.value:learningStandardReference:learningStandardId::string as learning_standard_id,
-        v_lsg.value:letterGradeEarned::string as letter_grade_earned,
-        v_lsg.value:numericGradeEarned::string as numeric_grade_earned,
+        v_lsg.value:letterGradeEarned::string as learning_standard_letter_grade_earned,
+        v_lsg.value:numericGradeEarned::string as learning_standard_numeric_grade_earned,
         {{ edu_edfi_source.extract_descriptor('v_lsg.value:performanceBaseConversionDescriptor::string') }} as performance_base_conversion_descriptor
     from stg_grades,
         lateral flatten(input=>v_learning_standard_grades, outer=>true) as v_lsg
