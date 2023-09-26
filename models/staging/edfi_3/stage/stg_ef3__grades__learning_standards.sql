1ef436cb1d9c4bd885120ef14126741ea2a1b9d9
Angelica Lastra <angelicaalastra@gmail.com>
2023-09-26 13:38:22 -0700

    revert grade changes

--- models/staging/edfi_3/stage/stg_ef3__grades__learning_standards.sql
+++ models/staging/edfi_3/stage/stg_ef3__grades__learning_standards.sql
@@ -4,10 +4,10 @@ with stg_grades as (
 ),
 flattened as (
     select
-        stg_grades.*,
+        stg_grades.* exclude(letter_grade_earned, numeric_grade_earned),
         v_lsg.value:learningStandardReference:learningStandardId::string as learning_standard_id,
-        v_lsg.value:letterGradeEarned::string as learning_standard_letter_grade_earned,
-        v_lsg.value:numericGradeEarned::string as learning_standard_numeric_grade_earned,
+        v_lsg.value:letterGradeEarned::string as letter_grade_earned,
+        v_lsg.value:numericGradeEarned::string as numeric_grade_earned,
         {{ edu_edfi_source.extract_descriptor('v_lsg.value:performanceBaseConversionDescriptor::string') }} as performance_base_conversion_descriptor
     from stg_grades,
         lateral flatten(input=>v_learning_standard_grades, outer=>true) as v_lsg
