f3c54201e2d29e0bf6a34593929bd0eda89530ae
ejoranlien <ejoranlien@edanalytics.org>
2023-10-02 16:32:02 -0500

    formatting

--- models/staging/edfi_3/base/base_ef3__learning_standards.sql
+++ models/staging/edfi_3/base/base_ef3__learning_standards.sql
@@ -14,20 +14,20 @@ renamed as (
         v:learningStandardId::string              as learning_standard_id,
         v:identificationCodes                     as v_learning_standard_identification_codes,
         -- descriptions
-        v:learningStandardItemCode::string        as learning_standard_item_code,
         {{ extract_descriptor('v:learningStandardCategoryDescriptor::string') }} as learning_standard_category,
-        {{ extract_descriptor('v:learningStandardScopeDescriptor::string') }} as learning_standard_scope,
+        {{ extract_descriptor('v:learningStandardScopeDescriptor::string') }}    as learning_standard_scope,
+        v:learningStandardItemCode::string        as learning_standard_item_code,
         v:courseTitle::string                     as course_title,
         v:description::string                     as learning_standard_description,
         v:academicSubjects                        as v_academic_subjects,
         v:contentStandard                         as v_content_standard,
         v:gradeLevels                             as v_grade_levels,
         v:prerequisiteLearningStandards           as v_prerequisite_learning_standards,
-        -- references
-        v:parentLearningStandardReference         as parent_learning_standard_reference,
         v:successCriteria::string                 as success_criteria,
         v:namespace::string                       as namespace,
         v:uri::string                             as uri,
+        -- references
+        v:parentLearningStandardReference         as parent_learning_standard_reference,
         -- edfi extensions
         v:_ext                                    as v_ext
     from learning_standards
