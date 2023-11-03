67f820518dfb37baf0b3932cd09af9ef39b4a2bb
ejoranlien <ejoranlien@edanalytics.org>
2023-11-02 21:35:57 -0500

    add SEA models

---
+++ models/staging/edfi_3/stage/stg_ef3__state_education_agencies.sql
@@ -0,0 +1,24 @@
+with base_state_education_agencies as (
+    select * from {{ ref('base_ef3__state_education_agencies') }}
+    where not is_deleted
+),
+keyed as (
+    select
+         {{ dbt_utils.surrogate_key(
+           ['tenant_code', 
+            'sea_id'] 
+        ) }} as k_sea,
+        base_state_education_agencies.*
+        {{ extract_extension(model_name=this.name, flatten=True) }}
+    from base_state_education_agencies
+),
+deduped as (
+    {{
+        dbt_utils.deduplicate(
+            relation='keyed',
+            partition_by='k_sea',
+            order_by='api_year desc, pull_timestamp desc'
+        )
+    }}
+)
+select * from deduped
