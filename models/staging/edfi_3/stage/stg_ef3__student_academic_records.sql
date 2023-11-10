02fffbbd2d56aa9117538b8ef95cee520f6bc2d0
sleblanc23 <sam23leblanc@gmail.com>
2023-11-10 13:59:44 -0600

    switch k_student to k_student_xyear

--- models/staging/edfi_3/stage/stg_ef3__student_academic_records.sql
+++ models/staging/edfi_3/stage/stg_ef3__student_academic_records.sql
@@ -13,9 +13,7 @@ keyed as (
                 'lower(academic_term)'
             ]
         ) }} as k_student_academic_record,
-        {{ gen_skey('k_student') }}, --todo: or k_student_xyear?
-        -- todo: can we use ed_org_ref to generate either a k_school or k_lea here
-        -- note: appears to be driven by the same logic as student_ed_org_assoc, so could use a single variable for both
+        {{ gen_skey('k_student_xyear') }},
         {{ edorg_ref(annualize=False) }},
         base_academic_records.*
         {{ extract_extension(model_name=this.name, flatten=True) }}
@@ -25,7 +23,7 @@ deduped as (
     {{
         dbt_utils.deduplicate(
             relation='keyed',
-            partition_by='k_student_academic_record, ed_org_id',
+            partition_by='k_student_academic_record',
             order_by='pull_timestamp desc'
         )
     }}
