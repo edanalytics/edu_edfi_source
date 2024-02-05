dc5a2a3429d59966a236d24d3d1789465e8e33e6
rlittle08 <rlittle@edanalytics.org>
2024-02-05 10:57:52 -0600

    add staff employment base/stg

---
+++ models/staging/edfi_3/base/base_ef3__staff_education_organization_employment_associations.sql
@@ -0,0 +1,37 @@
+with staff_ed_org_employ as (
+    {{ source_edfi3('staff_education_organization_employment_associations') }}
+),
+renamed as (
+    select 
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+        v:id::string as record_guid,
+        v:educationOrganizationReference:educationOrganizationId::int as ed_org_id,
+        v:educationOrganizationReference:link:rel::string             as ed_org_type,
+        v:staffReference:staffUniqueId::string                        as staff_unique_id,
+        v:credentialReference:credentialIdentifier::string            as credential_identifier,
+        v:department::string                                          as department,
+        v:hireDate::date                                              as hire_date,
+        v:endDate::date                                               as end_date,
+        v:fullTimeEquivalency::float                                  as full_time_equivalency,
+        v:hourlyWage::float                                           as hourly_wage,
+        v:annualWage::float                                           as annual_wage,
+        v:offerDate::date                                             as offer_date,
+        -- descriptors
+        {{ extract_descriptor('v:employmentStatusDescriptor::string') }}                                  as employment_status,
+        {{ extract_descriptor('v:credentialReference:stateOfIssueStateAbbreviationDescriptor::string') }} as credential_state,
+        {{ extract_descriptor('v:separationDescriptor::string') }}                                        as separation,
+        {{ extract_descriptor('v:separationReasonDescriptor::string') }}                                  as separation_reason,
+        -- references
+        v:credentialReference            as credential_reference,
+        v:educationOrganizationReference as education_organization_reference,
+        v:staffReference                 as staff_reference,
+        -- edfi extensions
+        v:_ext as v_ext
+    from staff_ed_org_employ
+)
+select * from renamed
---
+++ models/staging/edfi_3/stage/stg_ef3__staff_education_organization_employment_associations.sql
@@ -0,0 +1,24 @@
+with base_staff_ed_org_employ as (
+    select * from {{ ref('base_ef3__staff_education_organization_employment_associations') }}
+    where not is_deleted
+),
+keyed as (
+    select 
+        {{ gen_skey('k_staff') }},
+        --todo: edorg
+        {{ edorg_ref() }},
+        api_year as school_year,
+        base_staff_ed_org_employ.*
+        {{ extract_extension(model_name=this.name, flatten=True) }}
+    from base_staff_ed_org_employ
+),
+deduped as (
+    {{
+        dbt_utils.deduplicate(
+            relation='keyed',
+            partition_by= 'tenant_code, api_year, ed_org_id, employment_status, hire_date, staff_unique_id',
+            order_by='api_year desc, pull_timestamp desc'
+        )
+    }}
+)
+select * from deduped
