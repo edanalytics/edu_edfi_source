c495d2b4ec911e0d31189ff8642eacf6b8fefbf4
rlittle08 <rlittle@edanalytics.org>
2024-02-05 14:37:22 -0600

    add staff races

---
+++ models/staging/edfi_3/stage/stg_ef3__staff__races.sql
@@ -0,0 +1,14 @@
+with stage_staffs as (
+    select * from {{ ref('stg_ef3__staffs') }}
+),
+flattened as (
+    select 
+        tenant_code,
+        api_year,
+        k_staff,
+        {{ extract_descriptor('value:raceDescriptor::string') }} as race
+    from stage_staffs
+        , lateral flatten(input=>v_races)
+)
+select * from flattened
+
