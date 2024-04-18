c3d6e19f475e031df38461afe233b2f5d02ab9be
Susan Xiong <48801993+susanxiong@users.noreply.github.com>
2024-04-18 12:35:20 -0400

    Remove file_row_number alias for consistency

--- models/staging/edfi_3/base/base_ef3__student_academic_records.sql
+++ models/staging/edfi_3/base/base_ef3__student_academic_records.sql
@@ -7,7 +7,7 @@ renamed as (
         api_year,
         pull_timestamp,
         last_modified_timestamp,
-        file_row_number as row_number,
+        file_row_number,
         filename,
         is_deleted,
         v:id::string as record_guid,
