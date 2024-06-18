77ad4a89cc8b18d049c45b945b565dcf842408b5
rlittle08 <rlittle@edanalytics.org>
2024-06-18 09:13:04 -0500

    pull extensions out of nested sar models (#85)

--- models/staging/edfi_3/stage/stg_ef3__student_academic_records__gpas.sql
+++ models/staging/edfi_3/stage/stg_ef3__student_academic_records__gpas.sql
@@ -9,8 +9,18 @@ flattened as (
         {{ extract_descriptor('value:gradePointAverageTypeDescriptor::string') }} as gpa_type,
         value:gradePointAverageValue::float as gpa_value,
         value:isCumulative::boolean as is_cumulative,
-        value:maxGradePointAverageValue::float  as max_gpa_value
+        value:maxGradePointAverageValue::float  as max_gpa_value,
+        -- edfi extensions
+        value:_ext as v_ext 
     from stg_academic_records
         , lateral flatten(input=>v_grade_point_averages)
+),
+-- pull out extensions from v_gpas.v_ext to their own columns
+extended as (
+    select 
+        flattened.*
+        {{ extract_extension(model_name=this.name, flatten=True) }}
+
+    from flattened
 )
-select * from flattened
+select * from extended
