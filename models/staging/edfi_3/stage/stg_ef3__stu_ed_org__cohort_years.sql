eb9c238df54282ea82a6934cf0c902cd03ee55c9
keenzarate213 <kzarate@edanalytics.org>
2024-07-10 14:30:28 -0500

    flatten student cohort year

---
+++ models/staging/edfi_3/stage/stg_ef3__stu_ed_org__cohort_years.sql
@@ -0,0 +1,17 @@
+with stage_stu_ed_org as (
+    select * from {{ ref('stg_ef3__student_education_organization_associations') }}
+),
+flattened as (
+    select 
+        tenant_code,
+        api_year,
+        k_student,
+        k_student_xyear,
+        ed_org_id,
+        k_lea,
+        k_school,
+        {{ extract_descriptor('value:cohortYearTypeDescriptor::string') }} as cohort_year
+    from stage_stu_ed_org
+        , lateral flatten(input=>v_cohort_years)
+)
+select * from flattened
