e89c3af7818456c06a8629545bf01d7b15759a65
Maxim Tarasyuk <mtarasyuk@aristeksystems.com>
2024-07-18 16:50:41 +0300

    Base Evaluation (TPDM) models

---
+++ models/staging/tpdm/base/base_tpdm__evaluation_objectives.sql
@@ -0,0 +1,29 @@
+with evaluation_objectives as (
+    {{ source_edfi3('evaluation_objectives') }}
+),
+renamed as (
+    select
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+
+        v:id::string                                       as record_guid,
+        v:evaluationReference:educationOrganizationId::int as ed_org_id,
+        v:evaluationObjectiveTitle::string                 as evaluation_objective_title,
+        v:evaluationObjectiveDescription::string           as evaluation_objective_description,
+        v:sortOrder::int                                   as sort_order,
+        v:minRating::decimal(6,3)                          as min_rating,
+        v:maxRating::decimal(6,3)                          as max_rating,
+        -- descriptors
+        {{ extract_descriptor('v:evaluationTypeDescriptor::string') }} as evaluation_type,
+        -- unflattened lists
+        v:ratingLevels as rating_levels,
+        -- references
+        v:evaluationReference as evaluation_reference
+    from evaluation_objectives
+)
+select * from renamed
