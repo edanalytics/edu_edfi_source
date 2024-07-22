30ac2d8bbc12b12eb0dd628a33061914d966b087
sleblanc23 <sam23leblanc@gmail.com>
2024-07-22 12:01:31 -0500

    add student contact associations

---
+++ models/staging/edfi_3/stage/stg_ef3__student_contact_associations.sql
@@ -0,0 +1,34 @@
+with base_stu_contact as (
+    select * from {{ ref('base_ef3__student_contact_associations') }}
+    where not is_deleted
+),
+base_stu_parent as (
+    select * rename parent_reference as contact_reference
+    from {{ ref('base_ef3__student_parent_associations') }}
+    where not is_deleted
+),
+unioned as (
+    select * from base_stu_contact
+    union 
+    select * from base_stu_parent
+),
+keyed as (
+    select 
+        {{ gen_skey('k_student') }},
+        {{ gen_skey('k_contact') }},
+        {{ gen_skey('k_student_xyear') }},
+        api_year as school_year,
+        unioned.*
+        {{ extract_extension(model_name=this.name, flatten=True) }}
+    from unioned
+),
+deduped as (
+    {{
+        dbt_utils.deduplicate(
+            relation='keyed',
+            partition_by='k_student, k_contact',
+            order_by='pull_timestamp desc'
+        )
+    }}
+)
+select * from deduped
