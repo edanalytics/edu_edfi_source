520456ad348abe901c1f7ff530e912b3b88471bb
rlittle08 <rlittle@edanalytics.org>
2024-11-15 11:46:35 -0600

    Update gen_skey.sql

--- macros/gen_skey.sql
+++ macros/gen_skey.sql
@@ -211,7 +211,7 @@
         },
 
          'k_student_assessment': {
-            'reference_name': 'studentAssessmentReference'.
+            'reference_name': 'studentAssessmentReference',
             'col_list': [
                 'assessmentIdentifier',
                 'namespace',
