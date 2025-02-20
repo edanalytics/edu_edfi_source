b9c1507541cd6c3d8115f86c2fc281ed1f17bd49
rlittle08 <rlittle@edanalytics.org>
2025-02-20 10:51:26 -0600

    drafty handling deletes in incremental

--- models/staging/edfi_3/stage/stg_ef3__course_transcripts.sql
+++ models/staging/edfi_3/stage/stg_ef3__course_transcripts.sql
@@ -1,14 +1,28 @@
 {{ config(
     materialized='incremental',
-    unique_key=['k_course', 'k_student_academic_record', 'course_attempt_result']
+    unique_key=['k_course', 'k_student_academic_record', 'course_attempt_result'],
+    pre_hook=[
+        """
+        {% if is_incremental() %}
+        DELETE 
+        FROM {{ this }} as stg_data
+        USING {{ source('raw_edfi_3', '_deletes') }} deletes_data
+        WHERE deletes_data.name = '{{ this.table | replace(\"stg_ef3__\", \"\") }}'
+            and stg_data.tenant_code = deletes_data.tenant_code
+            and stg_data.api_year = deletes_data.api_year
+            and stg_data.record_guid = replace(get_ignore_case(deletes_data.v, 'id')::string, '-')
+            and deletes_data.pull_timestamp > (select max(pull_timestamp) from {{ this }})
+        ;
+        {% endif %}
+        """
+    ]
 ) }}
 with base_course_transcripts as (
     select * from {{ ref('base_ef3__course_transcripts') }}
-    where not is_deleted
 
     {% if is_incremental() %}
     -- Only get new or updated records since the last run
-    and last_modified_timestamp > (select max(pull_timestamp) from {{ this }})
+    where pull_timestamp > (select max(pull_timestamp) from {{ this }})
     {% endif %}
 ),
 keyed as (
@@ -24,8 +38,13 @@ deduped as (
         dbt_utils.deduplicate(
             relation='keyed',
             partition_by='k_course, k_student_academic_record, course_attempt_result',
-            order_by='api_year desc, pull_timestamp desc'
+            order_by='api_year desc, last_modified_timestamp desc'
         )
     }}
 )
 select * from deduped
+{# for incremental, deletes were already filtered up top #}
+{# Q before applying elsewhere: should we remove this conditional, since it's harmless to filter an already-filtered? or will that be slow? #}
+{% if not is_incremental() %}
+where not is_deleted
+{% endif %}
