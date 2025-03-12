a40ae3358df804b83807a48bd621526793c0e1ec
Angelica Lastra <angelicaalastra@gmail.com>
2025-03-11 21:25:49 -0700

    removed k_restraint_events as stage model won't be a dimension downstream

--- models/staging/edfi_3/stage/stg_ef3__restraint_events.sql
+++ models/staging/edfi_3/stage/stg_ef3__restraint_events.sql
@@ -7,15 +7,6 @@ keyed as (
         {{ gen_skey('k_student') }},
         {{ gen_skey('k_student_xyear') }},
         {{ gen_skey('k_school') }},
-        {{ dbt_utils.generate_surrogate_key(
-            [
-                'tenant_code',
-                'api_year',
-                'event_date',
-                'school_id',
-                'restraint_event_identifier'
-            ]
-        ) }} as k_restraint_event,
         api_year as school_year,
         *
         {{ extract_extension(model_name=this.name, flatten=True) }}
