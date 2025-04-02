0ffb66022df4d42e27570ecd8653f5e89b297d77
rlittle08 <rlittle@edanalytics.org>
2025-04-02 15:08:43 -0500

    handle xyear edge case

--- models/staging/edfi_3/stage/stg_ef3__staffs.sql
+++ models/staging/edfi_3/stage/stg_ef3__staffs.sql
@@ -13,14 +13,28 @@ keyed as (
         {{ extract_extension(model_name=this.name, flatten=True) }}
     from base_staffs
 ),
-deduped as (
+-- For x-year resources (those that do not include year in unique key), we need to first dedupe with year using last_modified_timestamp to keep deleted records if last record is deleted..
+deduped_within_year as (
     {{
         dbt_utils.deduplicate(
             relation='keyed',
+            partition_by='k_staff, api_year',
+            order_by='last_modified_timestamp desc, pull_timestamp desc'
+        )
+    }}    
+),
+-- .. then remove deletes as they shouldn't be used in x-year dedupe
+deduped_within_year_no_deletes as (
+    select * from deduped_within_year where not is_deleted
+),
+-- .. and then dedupe across years to enforce the correct grain, keeping latest year that wasn't deleted
+deduped_across_years as (
+    {{
+        dbt_utils.deduplicate(
+            relation='deduped_within_year_no_deletes',
             partition_by='k_staff',
             order_by='api_year desc, last_modified_timestamp desc, pull_timestamp desc'
         )
     }}
 )
-select * from deduped
-where not is_deleted
+select * from deduped_across_years
