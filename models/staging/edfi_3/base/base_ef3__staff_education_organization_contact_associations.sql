10c41b055cc1b33e163543364b7afbe6a566f000
Angelica Lastra <angelicaalastra@gmail.com>
2025-04-09 20:28:14 -0700

    added base and stage models for staff ed org contact associations

---
+++ models/staging/edfi_3/base/base_ef3__staff_education_organization_contact_associations.sql
@@ -0,0 +1,28 @@
+with staff_ed_org_contact_assoc as (
+    {{ source_edfi3('staff_education_organization_contact_associations') }}
+),
+renamed as (
+    select
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+        v:id::string                                                  as record_guid,
+        v:contactTitle::string                                        as contact_title,
+        v:educationOrganizationReference:educationOrganizationId::int as ed_org_id,
+        v:electronicMailAddress::string                               as electronic_mail_address,
+        v:staffReference:staffUniqueId::int                           as staff_unique_id,
+        -- arrays
+        v:telephones                                                  as v_telephones,
+        -- references
+        v:educationOrganizationReference:educationOrganizationId      as education_organization_reference,
+        v:staffReference                                              as staff_reference,
+        -- edfi extensions
+        v:_ext                                                        as v_ext
+
+    from staff_ed_org_contact_assoc
+)
+select * from renamed
---
+++ models/staging/edfi_3/stage/stg_ef3__staff_education_organization_contact_associations.sql
@@ -0,0 +1,23 @@
+with base_staff_ed_org_contact_assoc as (
+    select * from {{ ref('base_ef3__staff_education_organization_contact_associations') }}
+    where not is_deleted
+),
+keyed as (
+    select 
+        {{ gen_skey('k_staff') }},
+        {{ edorg_ref() }},
+        api_year as school_year,
+        base_staff_ed_org_contact_assoc.*
+        {{ extract_extension(model_name=this.name, flatten=True) }}
+    from base_staff_ed_org_contact_assoc
+),
+deduped as (
+    {{
+        dbt_utils.deduplicate(
+            relation='keyed',
+            partition_by= 'tenant_code, api_year, begin_date, ed_org_id, staff_unique_id, staff_classification',
+            order_by='api_year desc, pull_timestamp desc'
+        )
+    }}
+)
+select * from deduped
