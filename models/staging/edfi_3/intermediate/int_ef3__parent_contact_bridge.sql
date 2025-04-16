d66e29735a4c4541191f2248201a910dbc39ebd7
sleblanc23 <sleblanc@edanalytics.org>
2025-04-16 10:01:51 -0500

    use star macro, dedupe strategy from main

--- models/staging/edfi_3/intermediate/int_ef3__parent_contact_bridge.sql
+++
@@ -1,7 +0,0 @@
--- bridge differences between parents and contacts
-{{ config(materialized='view') }}
-with base_parents as (
-    select *, parent_unique_id as contact_unique_id
-    from {{ ref('base_ef3__parents') }}
-)
-select * from base_parents
--- models/staging/edfi_3/stage/stg_ef3__contacts.sql
+++ models/staging/edfi_3/stage/stg_ef3__contacts.sql
@@ -1,19 +1,17 @@
--- parents were renamed to contacts in Data Standard v5.0
-with unioned as (
-    {{
-        dbt_utils.union_relations(
-            relations=[
-                ref('base_ef3__contacts'), 
-                ref('int_ef3__parent_contact_bridge')
-            ],
-            exclude=['parent_unique_id']
-        )
-    }}
+with base_contacts as (
+    select * from {{ ref('base_ef3__contacts') }}
 ),
-drop_deletes as (
-    select * 
-    from unioned
-    where not is_deleted
+base_parents as (
+    select * from {{ ref('base_ef3__parents') }}
+),
+base_parents_renamed as (
+    {{ star('base_parents', rename=[['parent_unique_id', 'contact_unique_id']]) }}
+),
+-- parents were renamed to contacts in Data Standard v5.0
+unioned as (
+    select * from base_contacts
+    union 
+    select * from base_parents
 ),
 keyed as (
     select 
@@ -23,17 +21,34 @@ keyed as (
                 'lower(contact_unique_id)'
             ]
         ) }} as k_contact,
-        drop_deletes.*
+        unioned.*
         {{ extract_extension(model_name=[this.name, 'stg_ef3__parents'], flatten=True) }}
-    from drop_deletes
+    from unioned
 ),
-deduped as (
+-- For x-year resources (those that do not include year in unique key), there's an edge case 
+-- where a record we need for historic reporting could have been deleted in a later year. To avoid removing these,
+-- we need to first dedupe within year using last_modified_timestamp, then dedupe across years to get to a single record 
+deduped_within_year as (
     {{
         dbt_utils.deduplicate(
             relation='keyed',
-            partition_by='k_contact', 
-            order_by='api_year desc, pull_timestamp desc'
+            partition_by='k_contact, api_year',
+            order_by='last_modified_timestamp desc, pull_timestamp desc'
+        )
+    }}
+),
+-- .. then remove deletes as they shouldn't be used in x-year dedupe
+deduped_within_year_no_deletes as (
+    select * from deduped_within_year where not is_deleted
+),
+-- .. and then dedupe across years to enforce the correct grain, keeping latest year that wasn't deleted
+deduped_across_years as (
+    {{
+        dbt_utils.deduplicate(
+            relation='deduped_within_year_no_deletes',
+            partition_by='k_contact',
+            order_by='api_year desc'
         )
     }}
 )
-select * from deduped
+select * from deduped_across_years
