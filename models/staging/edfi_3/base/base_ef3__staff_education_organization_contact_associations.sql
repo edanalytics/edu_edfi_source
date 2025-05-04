9f5695c368e8eab6bd990964cb32c5bba24fa0e4
Angelica Lastra <angelicaalastra@gmail.com>
2025-05-04 14:55:07 -0700

    lower email address for future use

--- models/staging/edfi_3/base/base_ef3__staff_education_organization_contact_associations.sql
+++ models/staging/edfi_3/base/base_ef3__staff_education_organization_contact_associations.sql
@@ -13,7 +13,7 @@ renamed as (
         v:id::string                                                  as record_guid,
         v:contactTitle::string                                        as contact_title,
         v:educationOrganizationReference:educationOrganizationId::int as ed_org_id,
-        v:electronicMailAddress::string                               as email_address,
+        lower(v:electronicMailAddress::string)                        as email_address,
         v:staffReference:staffUniqueId::int                           as staff_unique_id,
         -- arrays
         v:telephones                                                  as v_telephones,
