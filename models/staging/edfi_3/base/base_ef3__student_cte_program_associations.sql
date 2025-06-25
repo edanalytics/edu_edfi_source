7fbb671eeec255b5df278ba1049c44efe240cece
mberrien-fitzsimons <mberrien@edanalytics.org>
2025-06-25 12:46:20 -0500

    updated with remaining columns from v5.2 handbook

--- models/staging/edfi_3/base/base_ef3__student_cte_program_associations.sql
+++ models/staging/edfi_3/base/base_ef3__student_cte_program_associations.sql
@@ -13,35 +13,37 @@ renamed as (
         filename,
         is_deleted,
 
-        v:id::string                                                            as record_guid, 
+        v:id::string                                                                    as record_guid, 
         ods_version, 
         data_model_version,
-        v:studentReference:studentUniqueId::int                                 as student_unique_id,
-        v:educationOrganizationReference:educationOrganizationId::int           as ed_org_id,
-        v:educationOrganizationReference:link:rel::string                       as ed_org_type,
-        v:programReference:educationOrganizationId::int                         as program_ed_org_id,
-        v:beginDate::date                                                       as program_enroll_begin_date, 
-        v:endDate::date                                                         as program_enroll_end_date, 
-        v:programReference:programName::string                                  as program_name,
-        v:studentReference:link:rel::string                                     as student_reference_rel,
+        v:studentReference:studentUniqueId::int                                         as student_unique_id,
+        v:educationOrganizationReference:educationOrganizationId::int                   as ed_org_id,
+        v:educationOrganizationReference:link:rel::string                               as ed_org_type,
+        v:programReference:educationOrganizationId::int                                 as program_ed_org_id,
+        v:beginDate::date                                                               as program_enroll_begin_date, 
+        v:endDate::date                                                                 as program_enroll_end_date, 
+        v:programReference:programName::string                                          as program_name,
+        v:studentReference:link:rel::string                                             as student_reference_rel,
 
-        v:nonTraditionalGenderStatus::boolean                                   as non_traditional_gender_status,
-        v:privateCTEProgram::boolean                                            as private_cte_program,
+        v:nonTraditionalGenderStatus::boolean                                           as non_traditional_gender_status,
+        v:privateCTEProgram::boolean                                                    as private_cte_program,
+        v:ServedOutsideOfRegularSession::boolean                                        as served_outside_of_regular_session
 
         -- descriptors
-        {{ extract_descriptor('v:technicalSkillsAssessmentDescriptor') }}       as technical_skills_assessment,
-        {{ extract_descriptor('v:programReference:programTypeDescriptor') }}    as program_type,
+        {{ extract_descriptor('v:technicalSkillsAssessmentDescriptor') }}               as technical_skills_assessment,
+        {{ extract_descriptor('v:programReference:programTypeDescriptor') }}            as program_type,
+        {{ extract_descriptor('v:ReasonExitedDescriptor:ReasonExitedDescriptorId') }}   as reason_exited
 
         -- references
-        v:educationOrganizationReference                                        as education_organization_reference,
-        v:programReference                                                      as program_reference, 
-        v:studentReference                                                      as student_reference,
+        v:educationOrganizationReference                                                as education_organization_reference,
+        v:programReference                                                              as program_reference, 
+        v:studentReference                                                              as student_reference,
 
         -- lists
-        v:cteProgramServices                                                    as v_cte_program_services, 
-        v:ctePrograms                                                           as v_cte_programs, 
-        v:programParticipationStatuses                                          as v_program_participation_statuses, 
-        v:services                                                              as v_services
+        v:cteProgramServices                                                            as v_cte_program_services, 
+        v:ctePrograms                                                                   as v_cte_programs, 
+        v:programParticipationStatuses                                                  as v_program_participation_statuses, 
+        v:services                                                                      as v_services
         
     from source_stu_programs
 )
