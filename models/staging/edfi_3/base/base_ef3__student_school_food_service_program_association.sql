323b617d9fb2115a132d1e6545317be501f16060
mberrien-fitzsimons <mberrien@edanalytics.org>
2025-06-26 10:24:57 -0500

    added additional columns from handbook

--- models/staging/edfi_3/base/base_ef3__student_school_food_service_program_association.sql
+++ models/staging/edfi_3/base/base_ef3__student_school_food_service_program_association.sql
@@ -13,35 +13,38 @@ renamed as (
         filename,
         is_deleted,
 
-        v:ods_version::string,
+        v:id::string                                                                as record_guid, 
+        ods_version::string,
         data_model_version,
-        v:id::string                                                  as record_guid, 
-        v:educationOrganizationReference:educationOrganizationId::int as ed_org_id,
-        v:educationOrganizationReference:link:rel::string             as ed_org_type,
-        TRY_TO_DATE(TRIM(v:beginDate::string), 'YYYY-MM-DD')          as begin_date,
-        TRY_TO_DATE(TRIM(v:endDate::string), 'YYYY-MM-DD')            as end_date,
-        v:programReference:educationOrganizationId::int               as program_ed_org_id, 
-        v:programReference:programName::string                        as program_name,
-        v:programReference:link:rel::string                           as program_reference_rel,
-        v:studentReference:studentUniqueId::int                       as student_unique_id,
-        v:studentReference:link:rel::string                           as student_reference_rel, 
+        v:studentReference:studentUniqueId::string                                  as student_unique_id,
+        v:educationOrganizationReference:educationOrganizationId::int               as ed_org_id,
+        v:educationOrganizationReference:link:rel::string                           as ed_org_type,
+        v:programReference:educationOrganizationId::int                             as program_ed_org_id, 
+        v:beginDate::date                                                           as begin_date,
+        v:endDate::date                                                             as end_date,
+        v:programReference:programName::string                                      as program_name,
+        v:studentReference:link:rel::string                                         as student_reference_rel, 
+        v:programReference:link:rel::string                                         as program_reference_rel,
 
+        v:directCertification::boolean                                              as direct_certification,
+        v:servedOutsideOfRegularSession::boolean                                    as served_outside_of_regular_session,
 
         -- descriptors
-        {{ extract_descriptor('v:programReference:programTypeDescriptor') }} as program_type_descriptor,
+        {{ extract_descriptor('v:programReference:programTypeDescriptor') }}        as program_type,
+        {{ extract_descriptor('v:reasonExitedDescriptor') }} as reason_exited,
 
         -- references
-        v:educationOrganizationReference                              as education_organization_reference,
-        v:programReference                                            as program_reference,
-        v:studentReference                                            as student_reference,
-
+        v:educationOrganizationReference                                            as education_organization_reference,
+        v:programReference                                                          as program_reference,
+        v:studentReference                                                          as student_reference,
 
         -- lists
-        v:programParticipationStatuses                                as program_participation_statuses,
-        v:schoolFoodServiceProgramServices                            as school_food_service_program_services,
+        v:programParticipationStatuses                                              as v_program_participation_statuses,
+        v:schoolFoodServiceProgramServices                                          as v_school_food_service_program_services,
 
         -- edfi extensions
-        v:_ext                                                        as v_ext
+        v:_ext                                                                      as v_ext
+
     from source_stu_program
 )
 
