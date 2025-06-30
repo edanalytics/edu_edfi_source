2f483db09115f3b842c732a9a181df41be3e23d0
oscarrezab <oscarrezabautista@gmail.com>
2025-06-30 13:04:31 -0500

    Create base and stage models for tpdm survey_section_response_person_target_associations and update their respective yml files.

---
+++ models/staging/tpdm/base/base_tpdm__survey_section_response_person_target_associations.sql
@@ -0,0 +1,29 @@
+with survey_section_response_person_target_associations as (
+    {{ source_edfi3('survey_section_response_person_target_associations') }}
+),
+renamed as (
+    select
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+
+        v:id::string as record_guid,
+        -- identity components
+        v:surveySectionResponseReference:namespace::string                           as namespace, 
+        v:personReference:personId::string                                           as person_id,
+        {{ extract_descriptor('v:personReference:sourceSystemDescriptor::string') }} as source_system,
+        v:surveySectionResponseReference:surveyIdentifier::string                    as survey_id,
+        v:surveySectionResponseReference:surveyResponseIdentifier::string            as survey_response_id,
+        v:surveySectionResponseReference:surveySectionTitle::string                  as survey_section_title, 
+        -- references
+        v:surveySectionResponseReference as survey_section_response_reference,
+        v:personReference         as person_reference,
+        -- edfi extensions
+        v:_ext as v_ext 
+    from survey_section_response_person_target_associations
+)
+select * from renamed
---
+++ models/staging/tpdm/stage/stg_tpdm__survey_section_response_person_target_associations.sql
@@ -0,0 +1,31 @@
+with survey_section_response_person_target_associations as (
+    select * from {{ ref('base_tpdm__survey_section_response_person_target_associations') }}
+),
+keyed as (
+    select 
+        {{ dbt_utils.generate_surrogate_key(
+            ['tenant_code',
+            'api_year',
+            'lower(namespace)',
+            'lower(person_id)',
+            'lower(source_system)',
+            'lower(survey_id)',
+            'lower(survey_response_id)',
+            'lower(survey_section_title)'] 
+        ) }} as k_survey_section_response_person_target_association,
+        {{ gen_skey('k_survey_section_response') }},  
+        {{ gen_skey('k_person') }},  
+        survey_section_response_person_target_associations.*
+        {{ extract_extension(model_name=this.name, flatten=True) }}  
+    from survey_section_response_person_target_associations
+),
+deduped as (
+    {{
+        dbt_utils.deduplicate(
+            relation='keyed',
+            partition_by='k_survey_section_response_person_target_association',
+            order_by='last_modified_timestamp desc, pull_timestamp desc')
+    }}
+)
+select * from deduped
+where not is_deleted
