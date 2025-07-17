db61ff6adc269db1a5d33b4011abcd6a78b38306
oscarrezab <oscarrezabautista@gmail.com>
2025-07-17 15:46:24 -0500

    Fix base model

--- models/staging/tpdm/base/base_tpdm__professional_development_events.sql
+++ models/staging/tpdm/base/base_tpdm__professional_development_events.sql
@@ -19,11 +19,9 @@ renamed as (
         v:multipleSession::boolean              as is_multiple_session,
         v:professionalDevelopmentReason::string as professional_development_reason,
         v:required::boolean                     as is_required,
-        v:totalHoura::integer                   as total_hours,  -- should this be float or number type instead?
+        v:totalHours::integer                   as total_hours,  -- should this be float or number type instead?
         -- descriptors
         {{ extract_descriptor('v:professionalDevelopmentOfferedByDescriptor::string') }} as offered_by,
-        -- references
-        v:openStaffPositionReference as open_staff_position_reference,
         -- edfi extensions
         v:_ext as v_ext
 
