be737f5c4bba2bb23529c583c7491e1145af55c9
oscarrezab <oscarrezabautista@gmail.com>
2025-07-19 14:11:53 -0500

    Create base/stg models for StudentPath

---
+++ models/staging/tpdm/base/base_tpdm__student_paths.sql
@@ -0,0 +1,29 @@
+with student_paths as (
+    {{ source_edfi3('student_paths') }}
+),
+renamed as (
+    select
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        filename,
+        file_row_number,
+        is_deleted,
+
+        v:id::string as record_guid,
+        -- identity components
+        v:pathReference:pathName::string as path_name,
+        v:studentReference:studentUniqueId::string as student_unique_id,
+        v:pathReference:educationOrganizationId::int as ed_org_id,
+        -- unflattened lists
+        v:periods as v_periods,
+        -- references
+        v:pathReference as path_reference,
+        v:studentReference as student_reference,
+        -- edfi extensions
+        v:_ext as v_ext
+
+    from student_paths
+)
+select * from renamed
---
+++ models/staging/tpdm/stage/stg_tpdm__student_paths.sql
@@ -0,0 +1,22 @@
+with base_student_paths as (
+    select * from {{ ref('base_tpdm__student_paths') }}
+),
+keyed as (
+    select
+        {{ gen_skey('k_path') }},
+        {{ gen_skey('k_student') }},
+        base_student_paths.*
+        {{ extract_extension(model_name=this.name, flatten=True) }}
+    from base_student_paths
+),
+deduped as (
+    {{
+        dbt_utils.deduplicate(
+            relation='keyed',
+            partition_by='k_path, k_student',
+            order_by='last_modified_timestamp desc, pull_timestamp desc'
+        )
+    }}
+)
+select * from deduped
+where not is_deleted
