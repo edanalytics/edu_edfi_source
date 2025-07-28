e6b292faa5b553d1513ecd5c22750b5225ac45cd
Jacob Bortell <jbortell@edanalytics.org>
2025-07-28 10:01:30 -0500

    Update generated program evaluations

--- models/staging/edfi_3/base/base_ef3__student_program_evaluations.sql
+++ models/staging/edfi_3/base/base_ef3__student_program_evaluations.sql
@@ -1,45 +1,38 @@
+with student_program_evaluations as (
+    select * from {{ source_edfi3('student_program_evaluations') }}
+)
 
-with base_ef3__student_program_evaluations as (
-    select * from {{ source('student_program_evaluations') }}
-),
-
-renamed as (
+, renamed as (
     select
-        tenant_code,
-        api_year,
-        pull_timestamp,
-        file_row_number,
-        last_modified_timestamp,
-        filename,
-        is_deleted,
-        ods_version,
-        data_model_version,
 
         -- generic
-        v:id::string                                                                 as record_guid,
-        v:evaluationDate::date                                                       as evaluation_date,
-        v:evaluationDuration::int                                                    as evaluation_duration,
-        v:summaryEvaluationNumericRating::float                                      as summary_evaluation_numeric_rating,
-        v:summaryEvaluationComment::string                                           as summary_evaluation_comment,
+        v:id::string                                                  as record_guid,
+        v:educationOrganizationReference:educationOrganizationId::int as ed_org_id,
+        v:educationOrganizationReference:link:rel::string             as ed_org_type,
+        v:evaluationDate::date                                        as evaluation_date,
+        v:evaluationDuration::int                                     as evaluation_duration,
+        v:studentReference:studentUniqueId::string                    as student_unique_id,
+        v:summaryEvaluationComment::string                            as summary_evaluation_comment,
+        v:summaryEvaluationNumericRating::float                       as summary_evaluation_numeric_rating,
 
         -- references
-        v:programEvaluationReference                                                 as program_evaluation_reference,
-        v:studentReference                                                           as student_reference,
-        v:educationOrganizationReference                                             as education_organization_reference,
-        v:staffEvaluatorStaffReference                                               as staff_evaluator_staff_reference,
+        v:educationOrganizationReference as education_organization_reference,
+        v:programEvaluationReference     as program_evaluation_reference,
+        v:staffEvaluatorStaffReference   as staff_evaluator_staff_reference,
+        v:studentReference               as student_reference,
+
+        -- lists
+        v:externalEvaluators          as v_external_evaluators,
+        v:studentEvaluationElements   as v_student_evaluation_elements,
+        v:studentEvaluationObjectives as v_student_evaluation_objectives,
 
         -- descriptors
         {{ extract_descriptor('v:summaryEvaluationRatingLevelDescriptor::string') }} as summary_evaluation_rating_level,
 
-        -- lists
-        v:externalEvaluators                                                         as v_external_evaluators,
-        v:studentEvaluationObjectives                                                as v_student_evaluation_objectives,
-        v:studentEvaluationElements                                                  as v_student_evaluation_elements,
-
         -- ed-fi extensions
-        v:_ext                                                                       as v_ext,
+        v:_ext as v_ext,
 
-    from base_ef3__student_program_evaluations
+    from student_program_evaluations
 )
 
 select * from renamed
