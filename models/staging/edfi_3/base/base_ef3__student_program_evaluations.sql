1f49e8ed3326ca13eaccb8b222924c7e215c4fd8
Jacob Bortell <jbortell@edanalytics.org>
2025-07-31 10:34:09 -0500

    Update new models

--- models/staging/edfi_3/base/base_ef3__student_program_evaluations.sql
+++ models/staging/edfi_3/base/base_ef3__student_program_evaluations.sql
@@ -13,14 +13,25 @@ with student_program_evaluations as (
         file_row_number,
         filename,
         is_deleted,
-        v:id::string                                                  as record_guid,
-        v:educationOrganizationReference:educationOrganizationId::int as ed_org_id,
-        v:educationOrganizationReference:link:rel::string             as ed_org_type,
-        v:evaluationDate::date                                        as evaluation_date,
-        v:evaluationDuration::int                                     as evaluation_duration,
-        v:studentReference:studentUniqueId::string                    as student_unique_id,
-        v:summaryEvaluationComment::string                            as summary_evaluation_comment,
-        v:summaryEvaluationNumericRating::float                       as summary_evaluation_numeric_rating,
+
+        v:id::string                                                     as record_guid,
+        v:educationOrganizationReference:educationOrganizationId::int    as ed_org_id,
+        v:educationOrganizationReference:link:rel::string                as ed_org_type,
+        v:evaluationDate::date                                           as evaluation_date,
+        v:evaluationDuration::int                                        as evaluation_duration,
+        v:programEvaluationReference:programEducationOrganizationId::int as program_evaluation_ed_org_id,
+        v:programEvaluationReference:programEvaluationTitle::string      as program_evaluation_title,
+        v:programEvaluationReference:programName::string                 as program_name,
+        v:staffEvaluatorStaffReference:staffUniqueId::string             as staff_unique_id,
+        v:studentReference:studentUniqueId::string                       as student_unique_id,
+        v:summaryEvaluationComment::string                               as summary_evaluation_comment,
+        v:summaryEvaluationNumericRating::float                          as summary_evaluation_numeric_rating,
+
+        -- descriptors
+        {{ extract_descriptor('v:programEvaluationReference:programEvaluationPeriodDescriptor::string') }} as program_evaluation_period_descriptor,
+        {{ extract_descriptor('v:programEvaluationReference:programEvaluationTypeDescriptor::string') }}   as program_evaluation_type_descriptor,
+        {{ extract_descriptor('v:programEvaluationReference:programTypeDescriptor::string') }}             as program_type_descriptor,
+        {{ extract_descriptor('v:summaryEvaluationRatingLevelDescriptor::string') }}                       as summary_evaluation_rating_level,
 
         -- references
         v:educationOrganizationReference as education_organization_reference,
@@ -32,9 +43,6 @@ with student_program_evaluations as (
         v:externalEvaluators          as v_external_evaluators,
         v:studentEvaluationElements   as v_student_evaluation_elements,
         v:studentEvaluationObjectives as v_student_evaluation_objectives,
-
-        -- descriptors
-        {{ extract_descriptor('v:summaryEvaluationRatingLevelDescriptor::string') }} as summary_evaluation_rating_level,
 
         -- ed-fi extensions
         v:_ext as v_ext,
--- models/staging/edfi_3/base/base_ef3__student_special_education_program_eligibility_associations.sql
+++ models/staging/edfi_3/base/base_ef3__student_special_education_program_eligibility_associations.sql
@@ -6,6 +6,14 @@ with student_special_education_program_eligibility_associations as (
     select
 
         -- generic
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+
         v:id::string                                                  as record_guid,
         v:consentToEvaluationDate::date                               as consent_to_evaluation_date,
         v:consentToEvaluationReceivedDate::date                       as consent_to_evaluation_received_date,
@@ -14,25 +22,28 @@ with student_special_education_program_eligibility_associations as (
         v:eligibilityConferenceDate::date                             as eligibility_conference_date,
         v:eligibilityDeterminationDate::date                          as eligibility_determination_date,
         v:eligibilityEvaluationDate::date                             as eligibility_evaluation_date,
-        v:evaluationCompleteIndicator::boolean                        as is_evaluation_complete_indicator,
+        v:evaluationCompleteIndicator::boolean                        as is_evaluation_complete,
         v:evaluationDelayDays::int                                    as evaluation_delay_days,
         v:evaluationLateReason::string                                as evaluation_late_reason,
-        v:ideaIndicator::boolean                                      as is_idea_indicator,
+        v:ideaIndicator::boolean                                      as is_idea,
         v:originalECIServicesDate::date                               as original_eci_services_date,
+        v:programReference:educationOrganizationId::int               as program_ed_org_id,
+        v:programReference:programName::string                        as program_name,
         v:studentReference:studentUniqueId::string                    as student_unique_id,
         v:transitionConferenceDate::date                              as transition_conference_date,
         v:transitionNotificationDate::date                            as transition_notification_date,
+
+        -- descriptors
+        {{ extract_descriptor('v:eligibilityDelayReasonDescriptor::string') }}       as eligibility_delay_reason,
+        {{ extract_descriptor('v:eligibilityEvaluationTypeDescriptor::string') }}    as eligibility_evaluation_type,
+        {{ extract_descriptor('v:evaluationDelayReasonDescriptor::string') }}        as evaluation_delay_reason,
+        {{ extract_descriptor('v:ideaPartDescriptor::string') }}                     as idea_part,
+        {{ extract_descriptor('v:programReference:programTypeDescriptor::string') }} as program_type_descriptor,
 
         -- references
         v:educationOrganizationReference as education_organization_reference,
         v:programReference               as program_reference,
         v:studentReference               as student_reference,
-
-        -- descriptors
-        {{ extract_descriptor('v:eligibilityDelayReasonDescriptor::string') }}    as eligibility_delay_reason,
-        {{ extract_descriptor('v:eligibilityEvaluationTypeDescriptor::string') }} as eligibility_evaluation_type,
-        {{ extract_descriptor('v:evaluationDelayReasonDescriptor::string') }}     as evaluation_delay_reason,
-        {{ extract_descriptor('v:ideaPartDescriptor::string') }}                  as idea_part,
 
         -- ed-fi extensions
         v:_ext as v_ext,
