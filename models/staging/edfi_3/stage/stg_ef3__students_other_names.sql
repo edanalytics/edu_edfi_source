5aad11d76bd4690f3fd591375af6970e390a56e7
gnguyen87 <gnguyen@macalester.edu>
2025-08-19 13:46:07 -0500

    draft stg_other_name model

---
+++ models/staging/edfi_3/stage/stg_ef3__students_other_names.sql
@@ -0,0 +1,38 @@
+with students as (
+    select * from {{ ref('stg_ef3__students') }}
+),
+flattened as (
+    select 
+        k_student,
+        k_student_xyear,
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+        record_guid,
+        ods_version,
+        data_model_version,
+        student_unique_id,
+        {{ extract_descriptor('value:otherNameTypeDescriptor::varchar') }} as otherNameType,
+        value:personalTitlePrefix::varchar as personalTitlePrefix,
+        value:firstName::varchar as firstName,
+        value:middleName::varchar as middleName,
+        value:lastSurname::varchar as lastSurname,
+        value:generationCodeSuffix::varchar as generationCodeSuffix
+    from students
+        {{ json_flatten('v_other_names') }}
+),
+deduped as (
+    {{
+        dbt_utils.deduplicate(
+            relation='flattened',
+            partition_by='k_student, otherNameType', 
+            order_by='last_modified_timestamp desc, pull_timestamp desc'
+        )
+    }}
+) 
+select * from deduped
+where not is_deleted
