3bb7579093cca09a30d86efeda7e5885f6da4154
Jacob Bortell <jbortell@edanalytics.org>
2025-10-01 13:04:45 -0500

    DS v5 Program Updates

---
+++ models/staging/edfi_3/base/base_ef3__program_evaluations.sql
@@ -0,0 +1,44 @@
+with program_evaluations as (
+    {{ source_edfi3('program_evaluations') }}
+),
+
+renamed as (
+    select
+
+        -- generic
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+        ods_version,
+        data_model_version,
+
+        v:id::string                                    as record_guid,
+        v:evaluationMaxNumericRating::float             as evaluation_max_numeric_rating,
+        v:evaluationMinNumericRating::float             as evaluation_min_numeric_rating,
+        v:programEvaluationDescription::string          as program_evaluation_description,
+        v:programEvaluationTitle::string                as program_evaluation_title,
+        v:programReference:educationOrganizationId::int as program_ed_org_id,
+        v:programReference:programName::string          as program_name,
+
+        -- descriptors
+        {{ extract_descriptor('v:programEvaluationPeriodDescriptor::string') }}      as program_evaluation_period,
+        {{ extract_descriptor('v:programEvaluationTypeDescriptor::string') }}        as program_evaluation_type,
+        {{ extract_descriptor('v:programReference:programTypeDescriptor::string') }} as program_type,
+
+        -- references
+        v:programReference as program_reference,
+
+        -- lists
+        v:levels as v_levels,
+
+        -- ed-fi extensions
+        v:_ext as v_ext
+
+    from program_evaluations
+)
+
+select * from renamed
---
+++ models/staging/edfi_3/stage/stg_ef3__student_special_education_program_eligibility_associations.sql
@@ -0,0 +1,31 @@
+with base_student_program_eligibility_associations as (
+    select * from {{ ref('base_ef3__student_special_education_program_eligibility_associations') }}
+),
+
+keyed as (
+    select
+        tenant_code,
+        api_year,
+
+        {{ gen_skey('k_student') }},
+        {{ gen_skey('k_student_xyear') }},
+        {{ gen_skey('k_program') }},
+        {{ edorg_ref() }},
+
+        base_student_program_eligibility_associations.*
+
+        {{ extract_extension(model_name=this.name, flatten=True) }}
+
+    from base_student_program_eligibility_associations
+),
+
+deduped as (
+    {{ dbt_utils.deduplicate(
+        relation='keyed',
+        partition_by='k_lea, k_school, k_student, k_program, consent_to_evaluation_received_date',
+        order_by='last_modified_timestamp desc, pull_timestamp desc'
+    ) }}
+)
+
+select * from deduped
+where not is_deleted
