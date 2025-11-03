8e921ed77933fe74cb2760689387abd3198b68c9
Ryan Aguilar <raguilar@edanalytics.org>
2025-11-03 12:54:42 -0500

    Clean up student_iep__service_deliveries

--- models/staging/sedm/base/base_sedm__student_iep_service_deliveries.sql
+++ models/staging/sedm/base/base_sedm__student_iep_service_deliveries.sql
@@ -12,39 +12,33 @@ renamed as (
         file_row_number,
         filename,
         is_deleted,
-
-        v:id::string                                                                      as record_guid,
+        v:id::string as record_guid,
         ods_version,
         data_model_version,
-        
-        -- identity components (composite key)
-        v:iepServiceDeliveryID::string                                                    as iep_service_delivery_id,
-        v:studentReference:studentUniqueId::string                                        as student_unique_id,
-        v:studentIEPReference:studentIEPAssociationID::string                             as student_iep_association_id,
-        v:studentIEPReference:educationOrganizationReference:educationOrganizationId::int as iep_servicing_ed_org_id,
-        v:studentIEPReference:iepFinalizedDate::date                                      as iep_finalized_date,
-        {{ extract_descriptor('v:serviceDeliveryDescriptor::string') }}                   as service_delivery,
-        v:serviceDeliveryDate::date                                                       as service_delivery_date,
-        
-        -- optional fields
-        v:studentIEPServicePrescriptionReference:iepServiceID::string                     as iep_service_prescription_id,
-        v:studentIEPServicePrescriptionReference:servicePrescriptionDate::date            as service_prescription_date,
-        {{ extract_descriptor('v:serviceProviderDescriptor::string') }}                   as service_provider_type,
-        v:serviceProviderStaffReference:staffUniqueId::string                             as service_provider_staff_unique_id,
-        
+        -- key columns
+        v:iepServiceDeliveryID::string                                    as iep_service_delivery_id,
+        v:studentReference:studentUniqueId::string                        as student_unique_id,
+        v:studentIEPReference:studentIEPAssociationID::string             as student_iep_association_id,
+        {{ extract_descriptor('v:serviceDeliveryDksescriptor::string') }} as service_delivery,
+        v:serviceDeliveryDate::date                                       as service_delivery_date,
+        -- value columns
+        v:studentIEPServicePrescriptionReference:iepServiceID::string          as iep_service_prescription_id,
+        v:studentIEPServicePrescriptionReference:servicePrescriptionDate::date as service_prescription_date,
+        v:serviceProviderStaffReference:staffUniqueId::string                  as service_provider_staff_unique_id,
+        -- descriptors
+        {{ extract_descriptor('v:serviceProviderDescriptor::string') }} as service_provider_type,
         -- references
-        v:studentReference                                                                as student_reference,
-        v:studentIEPReference                                                             as student_iep_reference,
-        v:studentIEPServicePrescriptionReference                                          as student_iep_service_prescription_reference,
-        v:serviceProviderStaffReference                                                   as service_provider_staff_reference,
-        v:ideaeventReferences                                                             as ideaevent_references,
-        
-        -- collections
-        v:serviceProviders                                                                as v_service_providers,
-        v:externalServiceProviders                                                        as v_external_service_providers,
+        v:studentReference                       as student_reference,
+        v:studentIEPReference                    as student_iep_reference,
+        v:studentIEPServicePrescriptionReference as student_iep_service_prescription_reference,
+        v:serviceProviderStaffReference          as service_provider_staff_reference,
+        v:ideaeventReferences                    as ideaevent_references,
+        -- lists
+        v:serviceProviders          as v_service_providers,
+        v:externalServiceProviders  as v_external_service_providers,
         
         -- edfi extensions
-        v:_ext                                                                            as v_ext
+        v:_ext as v_ext
 
     from source_student_iep_service_deliveries
 )
