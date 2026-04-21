b83e575f47cc5bc541f06165910b411a706491fe
rlittle08 <rlittle@edanalytics.org>
2026-04-20 21:24:22 -0500

    add dimensions not used by wi

---
+++ models/staging/edfi_3/base/base_ef3__operational_unit_dimensions.sql
@@ -0,0 +1,27 @@
+with operational_unit_dimensions as (
+    {{ source_edfi3('operational_unit_dimensions') }}
+),
+renamed as (
+    select
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+        v:id::string as record_guid,
+        ods_version,
+        data_model_version,
+        v:code::string as operational_unit_dimension_code,
+        v:fiscalYear::int as operational_unit_dimension_fiscal_year,
+        v:codeName::string as operational_unit_dimension_code_name,
+        -- nested lists
+        v:reportingTags::array as v_reporting_tags,
+
+        v:_lastModifiedDate::datetime as _last_modified_date,
+        -- edfi extensions
+        v:_ext as v_ext
+    from operational_unit_dimensions
+)
+select * from renamed
