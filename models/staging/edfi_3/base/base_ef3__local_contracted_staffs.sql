31f7b710047471f3ae3cab17685a0485e33345bf
rlittle08 <rlittle@edanalytics.org>
2026-04-20 21:23:00 -0500

    extend to more local types; add yml

---
+++ models/staging/edfi_3/base/base_ef3__local_contracted_staffs.sql
@@ -0,0 +1,36 @@
+with local_contracted_staffs as (
+    {{ source_edfi3('local_contracted_staffs') }}
+),
+renamed as (
+    select
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+        v:id::string as record_guid,
+        ods_version,
+        data_model_version,
+        -- key columns (local account + staff identify the row)
+        v:localAccountReference:accountIdentifier::string as local_account_identifier,
+        v:localAccountReference:educationOrganizationId::bigint as ed_org_id,
+        v:localAccountReference:link:rel::string as ed_org_type,
+        v:localAccountReference:fiscalYear::int as fiscal_year,
+        v:staffReference:staffUniqueId::string as staff_unique_id,
+        v:asOfDate::date as as_of_date,
+        -- value columns (some ODS builds use amountToDate instead of amount)
+        coalesce(v:amount::float, v:amountToDate::float) as amount,
+        -- descriptors
+        {{ extract_descriptor('v:financialCollectionDescriptor::string') }} as financial_collection,
+        -- references
+        v:localAccountReference as local_account_reference,
+        v:staffReference as staff_reference,
+
+        v:_lastModifiedDate::datetime as _last_modified_date,
+        -- edfi extensions
+        v:_ext as v_ext
+    from local_contracted_staffs
+)
+select * from renamed
---
+++ models/staging/edfi_3/base/base_ef3__local_encumbrances.sql
@@ -0,0 +1,34 @@
+with local_encumbrances as (
+    {{ source_edfi3('local_encumbrances') }}
+),
+renamed as (
+    select
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+        v:id::string as record_guid,
+        ods_version,
+        data_model_version,
+        -- key columns
+        v:localAccountReference:accountIdentifier::string as local_account_identifier,
+        v:localAccountReference:educationOrganizationId::bigint as ed_org_id,
+        v:localAccountReference:link:rel::string as ed_org_type,
+        v:localAccountReference:fiscalYear::int as fiscal_year,
+        v:asOfDate::date as as_of_date,
+        -- value columns
+        v:amount::float as amount,
+        -- descriptors
+        {{ extract_descriptor('v:financialCollectionDescriptor::string') }} as financial_collection,
+        -- references
+        v:localAccountReference as local_account_reference,
+
+        v:_lastModifiedDate::datetime as _last_modified_date,
+        -- edfi extensions
+        v:_ext as v_ext
+    from local_encumbrances
+)
+select * from renamed
