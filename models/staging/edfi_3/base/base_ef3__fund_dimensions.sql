7941ad0a4aeb5d1fecce8ba88090d4ecabe93f34
rlittle08 <rlittle@edanalytics.org>
2026-04-20 21:14:25 -0500

    base models used in Wi, standardized

---
+++ models/staging/edfi_3/base/base_ef3__fund_dimensions.sql
@@ -0,0 +1,27 @@
+with fund_dimensions as (
+    {{ source_edfi3('fund_dimensions') }}
+),
+renamed as (
+    select
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+        v:id::string as record_guid,
+        ods_version,
+        data_model_version,
+        v:code::string as fund_dimension_code,
+        v:fiscalYear::int as fund_dimension_fiscal_year,
+        v:codeName::string as fund_dimension_code_name,
+        -- nested lists
+        v:reportingTags::array as v_reporting_tags,
+
+        v:_lastModifiedDate::datetime as _last_modified_date,
+        -- edfi extensions
+        v:_ext as v_ext
+    from fund_dimensions
+)
+select * from renamed
---
+++ models/staging/edfi_3/base/base_ef3__local_budgets.sql
@@ -0,0 +1,33 @@
+with local_budgets as (
+    {{ source_edfi3('local_budgets') }}
+),
+renamed as (
+    select
+        tenant_code,
+        api_year,
+        pull_timestamp,
+        last_modified_timestamp,
+        file_row_number,
+        filename,
+        is_deleted,
+        v:id::string as record_guid,
+        ods_version,
+        data_model_version,
+        -- key columns
+        v:localAccountReference:accountIdentifier::string as local_account_identifier,
+        v:localAccountReference:educationOrganizationId::bigint as ed_org_id,
+        v:localAccountReference:link:rel::string as ed_org_type,
+        v:localAccountReference:fiscalYear::int as fiscal_year,
+        v:asOfDate::date as as_of_date,
+        -- value columns
+        v:amount::float as amount,
+        -- descriptors
+        {{ extract_descriptor('v:financialCollectionDescriptor::string') }} as financial_collection,
+        -- references
+        v:localAccountReference as local_account_reference,
+
+        v:_lastModifiedDate::datetime as _last_modified_date,
+        v:_ext as v_ext
+    from local_budgets
+)
+select * from renamed
