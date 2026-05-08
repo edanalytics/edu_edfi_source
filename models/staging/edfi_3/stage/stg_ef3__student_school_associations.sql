bcd6a58d7f5bb64572df670a92706414e73f2299
rlittle08 <rlittle@edanalytics.org>
2026-05-08 10:51:43 -0500

    shift to using existing derive macro, move it from edu_wh to here, move stu assess logic to stg

--- models/staging/edfi_3/stage/stg_ef3__student_school_associations.sql
+++ models/staging/edfi_3/stage/stg_ef3__student_school_associations.sql
@@ -2,7 +2,7 @@ with base_student_school_assoc as (
     select * from {{ ref('base_ef3__student_school_associations') }}
 ),
 keyed as (
-    select 
+    select
         {{ gen_skey('k_student') }},
         {{ gen_skey('k_student_xyear') }},
         {{ gen_skey('k_school') }},
@@ -12,18 +12,18 @@ keyed as (
         {{ extract_extension(model_name=this.name, flatten=True) }}
     from base_student_school_assoc
 ),
--- school_year is technically optional, so infer it if it's null to avoid breaking downstream joins
-with_inferred_school_year as (
-    select 
+-- school_year is technically optional in EdFi; infer from entry_date when missing
+with_derived_school_year as (
+    select
         {{ edu_edfi_source.star('keyed', except=['school_year']) }},
-        {{ edu_edfi_source.infer_school_year() }} as school_year
+        coalesce(school_year, {{ edu_edfi_source.derive_school_year('entry_date') }}) as school_year
     from keyed
 ),
 deduped as (
     {{
         dbt_utils.deduplicate(
-            relation='with_inferred_school_year',
-            partition_by='k_student, k_school, entry_date', 
+            relation='with_derived_school_year',
+            partition_by='k_student, k_school, entry_date',
             order_by='last_modified_timestamp desc, pull_timestamp desc'
         )
     }}
