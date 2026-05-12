b8b4a6309fe00383b6a28a932b27cee2b62be3e6
Samantha LeBlanc <56237580+sleblanc23@users.noreply.github.com>
2026-05-12 12:53:52 -0500

    feature/macro to infer null school years  (#181)
    * add macro to infer null school years
    
    * shift to using existing derive macro, move it from edu_wh to here, move stu assess logic to stg
    
    ---------
    
    Co-authored-by: rlittle08 <rlittle@edanalytics.org>

--- models/staging/edfi_3/stage/stg_ef3__student_assessments.sql
+++ models/staging/edfi_3/stage/stg_ef3__student_assessments.sql
@@ -31,10 +31,33 @@ keyed as (
     -- subsetting here because otherwise we would have referential integrity issues with k_assessment
     where academic_subject is not null
 ),
+-- school_year is technically optional in EdFi; infer from administration_date when missing,
+-- and apply the assessment dates xwalk when configured
+with_derived_school_year as (
+    select
+        {{ edu_edfi_source.star('keyed', except=['school_year']) }},
+        {% if var('edu:school_year:assessment_dates_xwalk_enabled', False) %}
+        iff(dates_xwalk.override_existing,
+            coalesce(dates_xwalk.school_year, keyed.school_year, {{ edu_edfi_source.derive_school_year('keyed.administration_date') }}),
+            coalesce(keyed.school_year, {{ edu_edfi_source.derive_school_year('keyed.administration_date') }}, dates_xwalk.school_year))
+        as school_year
+        {% else %}
+        coalesce(keyed.school_year, {{ edu_edfi_source.derive_school_year('keyed.administration_date') }}) as school_year
+        {% endif %}
+    from keyed
+    {% if var('edu:school_year:assessment_dates_xwalk_enabled', False) %}
+    left join {{ ref('xwalk_assessment_school_year_dates') }} dates_xwalk
+        -- note: between means A >= X AND A <= Y, so date upper/lower bounds should not overlap across years
+        on keyed.administration_date between dates_xwalk.start_date::date and dates_xwalk.end_date::date
+        -- allow school year cutoffs to differ by assessment, but also allow those fields to be null
+        and ifnull(dates_xwalk.assessment_identifier, '1') = iff(dates_xwalk.assessment_identifier is null, '1', keyed.assessment_identifier)
+        and ifnull(dates_xwalk.namespace, '1') = iff(dates_xwalk.namespace is null, '1', keyed.namespace)
+    {% endif %}
+),
 deduped as (
     {{
         dbt_utils.deduplicate(
-            relation='keyed',
+            relation='with_derived_school_year',
             partition_by='k_student_assessment',
             order_by='last_modified_timestamp desc, pull_timestamp desc'
         )
